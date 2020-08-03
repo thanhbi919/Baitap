d864ee3dc832bebccc3f0c2af4956aee0ed87d3d
thanhbi919 <thanhbi919@gmail.com>
2020-08-03 16:29:43 +0700

    bai 2.2

--- src/JVC/lesson2/activity22.java
+++ src/JVC/lesson2/activity22.java
@@ -1,4 +1,16 @@
 package JVC.lesson2;
 
+import java.util.Scanner;
+
 public class activity22 {
+    public static void main(String[] args) {
+        System.out.print("nhap n: ");
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int a= n;
+        int b=n*10+n;
+        int c=n*100+b;
+        System.out.println(a+"+"+b+"+"+c+"="+(a+b+c));
+
+    }
 }
