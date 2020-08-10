208d15d3c3459618dbb02e3c45263cdbf8c1d779
thanhbi919 <thanhbi919@gmail.com>
2020-08-10 23:33:41 +0700

    4.4

---
+++ src/JVC/lesson4/Activity4_4.java
@@ -0,0 +1,18 @@
+package JVC.lesson4;
+
+import java.util.Scanner;
+
+public class Activity4_4 {
+    public static void main(String[] args) {
+        System.out.print("Entry N: ");
+        Scanner scanner = new Scanner(System.in);
+        int n= scanner.nextInt();
+        int sum=0;
+        for(int i=1; i<=n; i=i+1){
+            sum=sum+i;
+        }
+        float j = sum,m=n;
+        System.out.println("The sum of 1 to "+n+" is "+sum);
+        System.out.println("The average is: "+j/m);
+    }
+}
