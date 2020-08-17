1b6e16826620ec4ab0e8cdcedcb61974a50f9c44
thanhbi919 <thanhbi919@gmail.com>
2020-08-17 08:11:22 +0700

    hihi

--- src/JVC/lesson6/test.java
+++ src/JVC/lesson6/test.java
@@ -1,4 +1,12 @@
 package JVC.lesson6;
 
 public class test {
+    public static void main(String[] args) {
+        String X[];
+        X = new String[1];
+        for(int i=0;i<3;i++){
+            X[i]="name";
+        }
+        System.out.println(X);
+    }
 }
