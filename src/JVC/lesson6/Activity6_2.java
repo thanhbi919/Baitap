6e81f2144d0e935cec34cc5e857ef1c7d0e0740d
thanhbi919 <thanhbi919@gmail.com>
2020-08-17 08:08:23 +0700

    StudentManagement

--- src/JVC/lesson6/Activity6_2.java
+++ src/JVC/lesson6/Activity6_2.java
@@ -1,4 +1,27 @@
 package JVC.lesson6;
 
 public class Activity6_2 {
+    public static void main(String[] args) {
+        // nhập thông tin sinh viên
+        StudentManagement x = new StudentManagement();
+
+        int pass = x.getPass();
+        //in ra số sinh viên qua môn
+        System.out.println("Số lượng sv qua môn: " + pass);
+
+        //in ra số sinh viên tạch môn
+        int fail = x.getFail();
+        System.out.println("Số lượng sv tạch môn: " + fail);
+
+        //in ra sinh viên có Gpa cao nhất
+        Student maxGPA = new Student();
+        maxGPA = x.maxGpa();
+        maxGPA.printInformaton();
+
+        //in ra sinh vien có Gpa thấp nhất
+        Student minGPA = new Student();
+        minGPA = x.minGpa();
+        minGPA.printInformaton();
+    }
+
 }
--- src/JVC/lesson6/StudentManagement.java
+++ src/JVC/lesson6/StudentManagement.java
@@ -1,4 +1,70 @@
 package JVC.lesson6;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StudentManagement {
+    private int total;
+    private Student[] students;
+
+    public StudentManagement() {//khoi tao danh sach sinh vien
+
+        System.out.print("Number of students want to enter: ");
+        Scanner scanner = new Scanner(System.in);
+        int total = scanner.nextInt();
+        this.total = this.total + total;
+        scanner.nextLine();
+
+        for (int i = 0; i <= this.total; i++) {//tai sao Student[0] lai khoi tao bang null
+            students = new Student[i];
+        }
+
+        for (int i = 0; i < this.total; i++) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            System.out.print("Gpa: ");
+            float gpa = scanner.nextFloat();
+            scanner.nextLine();
+            students[i] = new Student(name, gpa);
+        }
+    }
+
+    public int getPass() {//tra ve so sinh vien qua mon
+        int count = 0;
+        for (Student x : students) {
+            if (x.getGpa() >= 1.5) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getFail() {// tra ve so sinh vien truot mon
+        return total - getPass();
+    }
+
+    public Student maxGpa() {//tim sinh vien co GPA cao nhat
+        float max=0;
+        int count=0  ;
+        for (int i = 0; i < total; i++) {
+            if (students[i].getGpa() > max) {
+                max = students[i].getGpa();
+                count=i;
+            }
+        }
+        return students[count];
+    }
+
+    public Student minGpa() {//tim sinh vien co GPA nho nhat
+        float min = students[0].getGpa();
+        int count = 0;
+        for (int i = 1; i < total; i++) {
+            if (students[i].getGpa() <= min) {
+                min = students[i].getGpa();
+                count = i;
+            }
+        }
+        return students[count];
+    }
+
 }
